2c5ffc054f7184febdbb3c4f1d7122dca2857afd
David-Hidalgo <dehidalgo.22@est.ucab.edu.ve>
2024-07-12 14:00:48 -0400

    chat funciona
    Co-authored-by: Daniel Castellanos <dacastellanos.22@est.ucab.edu.ve>

--- public/src/js/mainAside.js
+++ public/src/js/mainAside.js
@@ -1,5 +1,9 @@
 
-document.querySelector("#header").outerHTML = `
+
+export function insertarHeader() {
+
+
+    document.querySelector("#header").outerHTML = `
     <header class="toolbar reserve-aside-space">
         <a href="/"><div class="logo">
                 <img alt="CinexUnidos" src="https://cinexunidos-production.up.railway.app/assets/images/cinex-unidos.png" width="50px"
@@ -26,7 +30,12 @@ document.querySelector("#header").outerHTML = `
             <option value="Puerto Ordaz">Puerto Ordaz</option>
         </select>
     </header>`;
-document.querySelector("#aside").outerHTML = `
+}
+
+export function insertarAside() {
+
+
+    document.querySelector("#aside").outerHTML = `
     <aside>
         <h2>Navegación</h2>
         <nav>
@@ -60,10 +69,10 @@ document.querySelector("#aside").outerHTML = `
                     </details>
                 </li>
                 <li>
-                    <a href="./seleccion_carameleria.html">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" stroke-width="25" fill="none" stroke="white" viewBox="0 0 448 512">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" stroke-width="25" fill="none" stroke="currentColor" viewBox="0 0 448 512">
                             <path d="M416 0C400 0 288 32 288 176V288c0 35.3 28.7 64 64 64h32V480c0 17.7 14.3 32 32 32s32-14.3 32-32V352 240 32c0-17.7-14.3-32-32-32zM64 16C64 7.8 57.9 1 49.7 .1S34.2 4.6 32.4 12.5L2.1 148.8C.7 155.1 0 161.5 0 167.9c0 45.9 35.1 83.6 80 87.7V480c0 17.7 14.3 32 32 32s32-14.3 32-32V255.6c44.9-4.1 80-41.8 80-87.7c0-6.4-.7-12.8-2.1-19.1L191.6 12.5c-1.8-8-9.3-13.3-17.4-12.4S160 7.8 160 16V150.2c0 5.4-4.4 9.8-9.8 9.8c-5.1 0-9.3-3.9-9.8-9L127.9 14.6C127.2 6.3 120.3 0 112 0s-15.2 6.3-15.9 14.6L83.7 151c-.5 5.1-4.7 9-9.8 9c-5.4 0-9.8-4.4-9.8-9.8V16zm48.3 152l-.3 0-.3 0 .3-.7 .3 .7z"/>
                         </svg>
+                    <a href="./seleccion_carameleria.html">
                         Caramelería
                     </a>
                 </li>
@@ -85,6 +94,17 @@ document.querySelector("#aside").outerHTML = `
                             </li>
                             <li>
                                 <div id="google_translate_element"></div>
+
+                                <script type="text/javascript">
+                                function googleTranslateElementInit() {
+                                new google.translate.TranslateElement({
+                                    pageLanguage: 'es',
+                                    includedLanguages: 'es,en' // remove this line if you want to include all languages
+                                },'google_translate_element');
+                                }
+                                </script>
+
+                                <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
                             </li>
                         </ol>
                     </details>
@@ -93,12 +113,9 @@ document.querySelector("#aside").outerHTML = `
         </nav>
         <br>
         <hr />
-        <footer>
-            <small> ¿Necesitas ayuda? </small>
-            <small> <a href="">Contáctanos</a> </small>
-        </footer>
+        
     </aside>`;
-
+}
 //const socket = io("https://cinexunidos-production.up.railway.app/chat");
 
 /* socket.on("connect", () => {
